d2216c3e8556e95f4ed48eb3de5d3713842a3319
Yuns <yuns.xie@qq.com>
2023-11-03 15:38:43 +0800

    ♻️ chore: drop rc-util

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import toArray from 'rc-util/lib/Children/toArray'
-import canUseDom from 'rc-util/lib/Dom/canUseDom'
-
 export { withLayouts, usePageProps } from './helpers/layouts'
 export { joinReactNodes } from './helpers/react-nodes'
-
-export { toArray, canUseDom }

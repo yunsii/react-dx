da37fe43501cf7793f1a3af914e53286f0ad857f
Yuns <yuns.xie@qq.com>
2023-11-03 11:26:24 +0800

    ✨ feat: export usePageProps

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import toArray from 'rc-util/lib/Children/toArray'
 import canUseDom from 'rc-util/lib/Dom/canUseDom'
 
-export { withLayouts } from './helpers/layouts'
+export { withLayouts, usePageProps } from './helpers/layouts'
 export { joinReactNodes } from './helpers/react-nodes'
 
 export { toArray, canUseDom }

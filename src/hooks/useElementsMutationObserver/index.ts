95ee78ab1d63491e3e308746cfad065e9a03d61d
Yuns <yuns.xie@qq.com>
2025-07-14 17:21:02 +0800

    🐛 fix: polish with react strict mode

--- src/hooks/useElementsMutationObserver/index.ts
+++ src/hooks/useElementsMutationObserver/index.ts
@@ -1,26 +1,30 @@
-import { useEffect, useRef } from 'react'
+import { createElementMutationObserver } from '@/helpers/dom/mutation-observer'
 import { omit } from 'lodash-es'
+import { useEffect, useRef } from 'react'
 
 import type { CreateElementMutationObserverOptions } from '@/helpers/dom/mutation-observer'
-
-import { createElementMutationObserver } from '@/helpers/dom/mutation-observer'
 
 export interface UseElementsMutationObserverPostElementOptions<
   E extends Element = Element,
   PE extends Element = Element,
 > extends Omit<
-CreateElementMutationObserverOptions<E>,
-'element'
-> {
+    CreateElementMutationObserverOptions<E>,
+    'element'
+  > {
   postElement: (element: E) => PE
+  /** 最外层监听器回调延迟执行间隔，默认 20ms */
+  timeout?: number
 }
 
 export interface UseElementsMutationObserverBaseOptions<
   E extends Element = Element,
 > extends Omit<
-CreateElementMutationObserverOptions<E>,
-'element'
-> {}
+    CreateElementMutationObserverOptions<E>,
+    'element'
+  > {
+  /** 最外层监听器回调延迟执行间隔，默认 20ms */
+  timeout?: number
+}
 
 export function useElementsMutationObserver<
   E extends Element = Element,
@@ -35,34 +39,52 @@ export function useElementsMutationObserver<E extends Element = Element>(
 ): void
 export function useElementsMutationObserver(selectors: string, options: UseElementsMutationObserverBaseOptions | UseElementsMutationObserverPostElementOptions) {
   const createdNodesRef = useRef<Node[]>([])
+  const subDisposerRef = useRef<(() => void) | undefined>()
+  const timerRef = useRef<number>()
 
   useEffect(() => {
     const callback = () => {
-      const targetElements = document.querySelectorAll(selectors)
+      timerRef.current = window.setTimeout(() => {
+        const targetElements = document.querySelectorAll(selectors)
 
-      targetElements.forEach((element) => {
-        const targetElement = 'postElement' in options ? options.postElement(element) : element
+        targetElements.forEach((element) => {
+          const targetElement = 'postElement' in options ? options.postElement(element) : element
 
-        if (createdNodesRef.current.includes(targetElement)) {
-          return
-        }
-        createdNodesRef.current.push(targetElement)
-        setTimeout(() => {
-          createdNodesRef.current = createdNodesRef.current.filter(
-            (item) => !!item.isConnected,
-          )
+          if (createdNodesRef.current.includes(targetElement)) {
+            subDisposerRef.current = createElementMutationObserver({
+              ...omit(options, 'postElement', 'onCreate'),
+              element: targetElement,
+            })
+          } else {
+            subDisposerRef.current = createElementMutationObserver({
+              ...omit(options, 'postElement'),
+              element: targetElement,
+            })
+          }
+
+          createdNodesRef.current.push(targetElement)
+          setTimeout(() => {
+            createdNodesRef.current = createdNodesRef.current.filter(
+              (item) => !!item.isConnected,
+            )
+          })
         })
-
-        createElementMutationObserver({
-          ...omit(options, 'postElement'),
-          element: targetElement,
-        })
-      })
+      }, typeof options.timeout === 'undefined' ? 20 : options.timeout)
     }
 
-    createElementMutationObserver({
+    const disposer = createElementMutationObserver({
       element: document.documentElement,
       onEffect: callback,
     })
-  }, [])
+
+    return () => {
+      disposer?.()
+      subDisposerRef.current?.()
+      createdNodesRef.current = []
+      if (timerRef.current) {
+        clearTimeout(timerRef.current)
+        timerRef.current = undefined
+      }
+    }
+  }, [selectors, options])
 }
